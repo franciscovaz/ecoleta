e66379a9af0bf040242c52e1e0206709f961f33b
Francisco Vaz <ftomasvaz@gmail.com>
2020-06-02 14:36:36 +0100

    create connection with knexjs

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/users", (req, res) => {
-  return res.json({ message: "Hello World 2" });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
