24e2625d5f118ead9788dff4d8c49a8b91aead30
Francisco Vaz <ftomasvaz@gmail.com>
2020-06-02 23:45:36 +0100

    list specific colect point show route

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -1,7 +1,23 @@
-import { Request, Response } from 'express';
+import { Request, Response, response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const point = await knex('points').where('id', id).first();
+
+    if (!point) {
+      return response.status(400).json({ message: 'Point not found.' });
+    }
+
+    const items = await knex('items')
+      .join('point_items', 'items.id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title');
+
+    return res.json({ point, items });
+  }
   async create(req: Request, res: Response) {
     const {
       name,
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import knex from './database/connection';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
@@ -11,5 +10,6 @@ const itemsController = new ItemsController();
 routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
