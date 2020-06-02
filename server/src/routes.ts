386c16448e6ee4df7463db77f07a7fb9c88ad2c0
Francisco Vaz <ftomasvaz@gmail.com>
2020-06-02 23:25:51 +0100

    separate Points route into controller

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import knex from './database/connection';
 
+import PointsController from './controllers/PointsController';
+
 const routes = express.Router();
+const pointsController = new PointsController();
 
 routes.get('/items', async (req, res) => {
   const items = await knex('items').select('*');
@@ -16,44 +19,6 @@ routes.get('/items', async (req, res) => {
   return res.json(serializedItems);
 });
 
-routes.post('/points', async (req, res) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items,
-  } = req.body;
-
-  //transaction - se a primeira falhar, a segunda nao executa. Dependem uma da outra
-  const trx = await knex.transaction();
-
-  const insertedIds = await trx('points').insert({
-    image: 'image-fake',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-  });
-
-  const point_id = insertedIds[0];
-
-  const pointItems = items.map((item_id: number) => {
-    return {
-      item_id,
-      point_id,
-    };
-  });
-
-  await trx('point_items').insert(pointItems);
-
-  return res.json({ success: true });
-});
+routes.post('/points', pointsController.create);
 
 export default routes;
