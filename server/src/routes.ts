790237cad405e30d8edca0969fef2aa82012b628
Francisco Vaz <ftomasvaz@gmail.com>
2020-06-02 23:16:03 +0100

    post points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -16,4 +16,44 @@ routes.get('/items', async (req, res) => {
   return res.json(serializedItems);
 });
 
+routes.post('/points', async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = req.body;
+
+  //transaction - se a primeira falhar, a segunda nao executa. Dependem uma da outra
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id,
+    };
+  });
+
+  await trx('point_items').insert(pointItems);
+
+  return res.json({ success: true });
+});
+
 export default routes;
