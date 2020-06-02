26f10e2d024de4335f4b084427112c2250279e76
Francisco Vaz <ftomasvaz@gmail.com>
2020-06-02 16:04:32 +0100

    create first migration create points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello World' });
 });
 
 export default routes;
