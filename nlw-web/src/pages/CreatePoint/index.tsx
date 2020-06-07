fdf7274f8220d3e53f848eb0bba28adabc7902a1
Francisco Vaz <ftomasvaz@gmail.com>
2020-06-07 15:25:30 +0100

    send function to dropzone component. create point with formdata instahead json

--- nlw-web/src/pages/CreatePoint/index.tsx
+++ nlw-web/src/pages/CreatePoint/index.tsx
@@ -55,6 +55,7 @@ const CreatePoint: React.FC = () => {
   });
 
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
 
@@ -139,16 +140,20 @@ const CreatePoint: React.FC = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
+
+    if (selectedFile) {
+      data.append('image', selectedFile);
+    }
 
     await api.post('points', data);
 
@@ -173,7 +178,7 @@ const CreatePoint: React.FC = () => {
           Registo do <br /> ponto de recolha
         </h1>
 
-        <Dropzone />
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
